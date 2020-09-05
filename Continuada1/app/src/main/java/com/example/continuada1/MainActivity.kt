3609a73945243ee8cb889ed2615a4cb1bdae2d57
Caporiccii <dmarco792@gmail.com.br>
2020-09-05 00:42:52 -0300

    Create rentability validation

--- Continuada1/app/src/main/java/com/example/continuada1/MainActivity.kt
+++ Continuada1/app/src/main/java/com/example/continuada1/MainActivity.kt
@@ -1,21 +1,92 @@
 package com.example.continuada1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.makeText
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    var isValid = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //apagar comentarios
-        //calcular tempo de quarentena de cada pessoa
-        //dias que não sai de casa
-        //dias que saiu
-        //verde para acima de 100 horas amarelo 50 vermelho 20
+//rentabilidade
+            }
+    fun onClick(view:View){
+
+        var investmentType = et_investido.text.toString()
+        var valueInvestment:String = et_compra.text.toString()
+        var valueActual = et_atual.text.toString()
+
+        validateFieldTypeInvestment(investmentType)
+        validateFieldValueInvested(valueInvestment)
+        validateFieldActualValue(valueActual)
+        calculatesRentability(valueInvestment, valueActual)
+
     }
-    fun aoClicar(view:View){
-        tv_texto_resultado.text = "Você ficou ${14} dias de quarentena"
-    }
+  fun validateFieldTypeInvestment(typeInvestment:String):String{
+
+      if(typeInvestment.equals("TD") || typeInvestment.equals("TD ") ||
+          typeInvestment.equals("RV") || typeInvestment.equals("RV ")) {
+          Toast.makeText(this, "Proximo Campo", Toast.LENGTH_SHORT).show()
+      }
+      else
+          Toast.makeText(this, "Tipo Invalido", Toast.LENGTH_SHORT).show()
+
+return typeInvestment
+      }
+
+    fun validateFieldValueInvested(valueInvestment:String):String{
+
+        try {
+    if(valueInvestment.toDouble() <= 0 || valueInvestment.isNullOrEmpty())
+        Toast.makeText(this, "Valor não pode ser menor que 0", Toast.LENGTH_LONG).show()
 }
+        catch (a:Exception){
+
+        }
+
+        return valueInvestment
+    }
+
+    fun validateFieldActualValue(valueActual:String):String{
+
+        try {
+            if(valueActual.toDouble() <= 0 || valueActual.isNullOrEmpty())
+                Toast.makeText(this, "Valor atual não pode ser menor que 0", Toast.LENGTH_LONG).show()
+        }
+        catch (a:Exception){
+
+        }
+
+        return valueActual
+    }
+
+    fun calculatesRentability(valorCompra:String, valorAtual:String){
+        var a =validateFieldActualValue(valorAtual)
+        var rentabilidadeNegativa = 0.0
+        var rentabilidadePositiva = 0.0
+
+      if(valorCompra > valorAtual) {
+          tv_texto_rentabilidade.setTextColor(Color.RED)
+          rentabilidadeNegativa = valorCompra.toDouble() - valorAtual.toDouble()
+          tv_texto_rentabilidade.text = "Sua rentabilidade é: - R$ ${rentabilidadeNegativa} "
+      }
+        else if(valorCompra < valorAtual){
+          tv_texto_rentabilidade.setTextColor(Color.GREEN)
+          rentabilidadePositiva = valorAtual.toDouble() - valorCompra.toDouble()
+          tv_texto_rentabilidade.text = "Sua rentabilidade é: + R$ ${rentabilidadePositiva}"
+      }
+        else{
+          tv_texto_rentabilidade.setTextColor(Color.parseColor("#FFCA33"))
+          tv_texto_rentabilidade.text = "Não houve mudanças na rentabilidade, Saldo: R$ ${valorAtual}"
+      }
+    }
+ }
+
+
